0dc1c18d1b9c7d03ca7e05fbaf6c222fd6940d52
Melvin Laplanche <melvin.wont.reply@gmail.com>
2019-04-20 18:37:04 -0700

    use CODECOV_TOKEN

--- tools/test.sh
+++ tools/test.sh
@@ -5,5 +5,5 @@ set -ex
 go test -v -race -coverprofile=coverage.txt -covermode=atomic .
 
 if [ "$CI" == "on" ]; then
-  include_cov=coverage.txt bash <(curl -s https://codecov.io/bash)
+  include_cov=coverage.txt bash <(curl -s https://codecov.io/bash) -t "$CODECOV_TOKEN"
 fi
